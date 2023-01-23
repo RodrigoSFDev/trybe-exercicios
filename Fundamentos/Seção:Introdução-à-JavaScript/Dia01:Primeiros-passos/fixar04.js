07663cef01e9ff2d90607ff7e8dc5117cebb5429
Rodrigo Silva Ferreira <rodrigosferreira0@gmail.com>
2023-01-23 10:37:32 -0300

    para fixar 04 atualizado

--- Fundamentos/Seção:Introdução-à-JavaScript/Dia01:Primeiros-passos/fixar04.js
+++ Fundamentos/Seção:Introdução-à-JavaScript/Dia01:Primeiros-passos/fixar04.js
@@ -1,14 +1,14 @@
-const currentHour = 3;
-let messege = '';
-if (currentHour >= 22){
-    messege = 'Não deveríamos comer nada, é hora de dormir';
+const currentHour = 'peixe';
+let message = '';
+if (currentHour >= 22 && currentHour <= 24 || currentHour < 4 && currentHour >= 0){
+    message = 'Não deveríamos comer nada, é hora de dormir';
 } else if (currentHour >=18 && currentHour < 22){
-    messege = 'Rango da noite, vamos jantar :D';
+    message = 'Rango da noite, vamos jantar :D';
 } else if (currentHour >= 14 && currentHour < 18){
-    messege = 'Vamos fazer um bolo pro café da tarde?';
+    message = 'Vamos fazer um bolo pro café da tarde?';
 } else if(currentHour >= 11 && currentHour < 14) {
-    messege = 'Hora do almoço!!!';
+    message = 'Hora do almoço!!!';
 } else if (currentHour >=4 && currentHour < 11){
-    messege = 'Hmmm, cheiro de café recém-passado';
-}
-console.log(messege);
+    message = 'Hmmm, cheiro de café recém-passado';
+} else{console.log('digite uma hora valida');}
+console.log(message);
